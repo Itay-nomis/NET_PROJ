88d09eebd93a12c6cdc41ce6dd9a461035e74f92
Dor <dormitnik16@gmail.com>
2025-01-06 14:28:17 +0200

    fixed login route problem

--- backend/services.py
+++ backend/services.py
@@ -1,14 +1,27 @@
+import smtplib
+import random
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from database.models import User
 from utils import encrypt_password
 
+# HARDCODED VARIABLES (MAY MOVE TO CONF FILE)
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+EMAIL_ADDRESS = '<EMAIL>'
+EMAIL_PASSWORD = '<PASSWORD>'
+
 
 def check_login(username: str, password: str, db: Session) -> bool:
     hashed_password = encrypt_password(password)
-    user = db.query(User).filter(User.username == username and User.password == hashed_password).first()
+    user = db.query(User).filter(and_(User.username == username, User.password == hashed_password)).first()
     return bool(user)
+
 
 def register_user(username: str, password: str, email: str, db: Session) -> JSONResponse:
     try:
@@ -18,7 +31,7 @@ def register_user(username: str, password: str, email: str, db: Session) -> JSON
             db.commit()
             db.refresh(user)
             return JSONResponse(status_code=201, content={"message": "User registered successfully"})
-        return JSONResponse(status_code=400, content={"message": "Weak password"}) # TODO add reason
+        return JSONResponse(status_code=400, content={"message": "Weak password"})  # TODO add reason
     except Exception as e:
         print(e)
         return JSONResponse(status_code=400, content={"message": "Something went wrong"})
@@ -28,6 +41,29 @@ def check_password(password: str) -> bool:
     return True
     # TODO Read file password configuration and check that the password is correctly defined
 
+
+def send_recovery_code(email: str):
+    recovery_code = f"{random.randint(100000, 99999)}"
+
+    subject = "Password recovery code"
+    body = "Your password recovery code is {recovery_code}"
+
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body))
+
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
+        server.quit()
+        return recovery_code
+    except Exception as e:
+        print(f"Failed to send mail: {e}")
+        return JSONResponse(status_code=400, content={"message": "Something went wrong"})
 
 # TODO forgot password -> DOR
 # TODO make sure uniqueness on register -> OMRI
